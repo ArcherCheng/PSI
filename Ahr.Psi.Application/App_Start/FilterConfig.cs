8184ce7864e5cb57b4033a399942192534750cdc
ArcherCheng <archer.cheng@msa.hinet.net>
2019-09-12 09:53:58 +0800

    加入專案檔案。

---
+++ Ahr.Psi.Application/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Ahr.Psi.Application
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Ahr.Psi.Application/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ahr.Psi.Application.MvcApplication" Language="C#" %>
